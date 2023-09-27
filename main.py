9e4c50d76d6454c8d06d86cee1f61b194edcec26
Makena-WB <makenawahu@gmail.com>
2023-09-27 19:37:47 +0300

    Added the script to calculate the similarity as well as making necessary installations for the packages to be used

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import json
+from transformers import LaBSE, LaBSETokenizer
 
 
 
@@ -54,6 +56,54 @@ with open(log_file_path, 'a') as log_file:
 
 print(f'Number of Male Students: {num_male_students}')
 print(f'Number of Female Students: {num_female_students}')
+
+
+# Load male and female names from TSV and CSV files
+male_names = pd.read_csv('male_names.csv')['name'].tolist()
+female_names = pd.read_csv('female_names.tsv', sep='\t')['name'].tolist()
+
+# Preprocess names
+tokenizer = LaBSETokenizer.from_pretrained("setu4993/LaBSE")
+male_names = [tokenizer.tokenize(name.lower()) for name in male_names]
+female_names = [tokenizer.tokenize(name.lower()) for name in female_names]
+
+# Load LaBSE model
+model = LaBSE.from_pretrained("setu4993/LaBSE")
+
+# Calculate similarity scores
+similarity_matrix = []
+for male_name in male_names:
+    row = []
+    for female_name in female_names:
+        inputs = tokenizer.encode_plus(
+            male_name, female_name, return_tensors="pt", padding=True, truncation=True
+        )
+        with torch.no_grad():
+            embeddings = model(**inputs).pooler_output
+        similarity_score = torch.nn.functional.cosine_similarity(embeddings[0], embeddings[1], dim=0)
+        row.append(similarity_score.item())
+    similarity_matrix.append(row)
+
+# Filter results with at least 50% similarity
+filtered_results = []
+for i, male_name in enumerate(male_names):
+    for j, female_name in enumerate(female_names):
+        if similarity_matrix[i][j] >= 0.5:
+            filtered_results.append({
+                "male_name": male_name,
+                "female_name": female_name,
+                "similarity_score": similarity_matrix[i][j]
+            })
+
+# Save filtered results in a JSON file
+with open('similarity_results.json', 'w') as f:
+    json.dump(filtered_results, f)
+
+
+
+
+
+
 
 
 
